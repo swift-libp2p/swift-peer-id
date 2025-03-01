940b881d5840b33c6099e2ae1c406c6b3a9dd122
Brandon <32753167+btoms20@users.noreply.github.com>
2025-02-28 20:54:48 -0800

    formatted and added license header

--- Package.swift
+++ Package.swift
@@ -1,5 +1,17 @@
 // swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-libp2p open source project
+//
+// Copyright (c) 2022-2025 swift-libp2p project authors
+// Licensed under MIT
+//
+// See LICENSE for license information
+// See CONTRIBUTORS for the list of swift-libp2p project authors
+//
+// SPDX-License-Identifier: MIT
+//
+//===----------------------------------------------------------------------===//
 
 import PackageDescription
 
@@ -7,20 +19,25 @@ let package = Package(
     name: "swift-peer-id",
     platforms: [
         .macOS(.v10_15),
-        .iOS(.v13)
+        .iOS(.v13),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "PeerID",
-            targets: ["PeerID"]),
+            targets: ["PeerID"]
+        )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/swift-libp2p/swift-libp2p-crypto.git", .upToNextMinor(from: "0.1.1")),
         .package(url: "https://github.com/swift-libp2p/swift-multihash.git", .upToNextMajor(from: "0.0.1")),
         .package(url: "https://github.com/swift-libp2p/swift-cid.git", .upToNextMajor(from: "0.0.1")),
-        .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", .upToNextMajor(from: "1.12.0"))
+        .package(
+            name: "SwiftProtobuf",
+            url: "https://github.com/apple/swift-protobuf.git",
+            .upToNextMajor(from: "1.12.0")
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -34,10 +51,12 @@ let package = Package(
                 .product(name: "SwiftProtobuf", package: "SwiftProtobuf"),
             ],
             resources: [
-              .copy("Protobufs/PeerIdProto.proto")
-            ]),
+                .copy("Protobufs/PeerIdProto.proto")
+            ]
+        ),
         .testTarget(
             name: "PeerIDTests",
-            dependencies: ["PeerID"]),
+            dependencies: ["PeerID"]
+        ),
     ]
 )
